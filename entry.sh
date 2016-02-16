b9b72a3f58d620d87a8f21388d0dd0f1543b0d72
Jibin George <mail@jibingeorge.com>
2016-02-16 13:27:27 +0530

    added support for SAN. change in CN

--- entry.sh
+++ entry.sh
@@ -7,7 +7,6 @@ locality=Bangalore
 organization="103 Studios Pvt Ltd"
 organizationalunit=HippyCam
 email=jibin@hhippycam.com
-commonname="HippyCam Root CA"
 
 #Optional
 password=dummypassword
@@ -22,33 +21,36 @@ case $1 in
             -key /crt/ca.key \
             -new -x509 -days 7300 -sha256 -extensions v3_ca \
             -out /crt/ca.crt \
-            -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+            -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=HippyDev Root CA/emailAddress=$email"
     ;;
 
     gen_crt)
       shift
-      commonname=$1
-      mkdir -p /crt/$commonname
+      domain=$1
+      mkdir -p /crt/$domain
 
+      (cat openssl.cnf; echo "DNS.3 = $domain"; echo "DNS.4 = *.$domain") > openssl.cnf.tmp
+      mv openssl.cnf.tmp openssl.cnf
+      cat openssl.cnf
       openssl genrsa -aes256 \
             -passout pass:$password \
-            -out /crt/$commonname/server.key 2048
+            -out /crt/$domain/server.key 2048
 
-      openssl rsa -in /crt/$commonname/server.key -passin pass:$password -out /crt/$commonname/server.key
+      openssl rsa -in /crt/$domain/server.key -passin pass:$password -out /crt/$domain/server.key
 
       openssl req -config openssl.cnf \
-            -key /crt/$commonname/server.key \
-            -new -sha256 -out /crt/$commonname/server.csr \
-            -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/CN=*.$commonname/emailAddress=$email"
+            -key /crt/$domain/server.key \
+            -new -sha256 -out /crt/$domain/server.csr \
+            -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=HippyDev/emailAddress=$email"
 
       openssl ca -config openssl.cnf -batch \
-            -extensions server_cert -days 3750 -notext -md sha256 \
-            -in /crt/$commonname/server.csr \
-            -out /crt/$commonname/server.crt
+            -extensions server_cert -extensions v3_req -days 3750 -notext -md sha256 \
+            -in /crt/$domain/server.csr \
+            -out /crt/$domain/server.crt
 
-      cat /crt/$commonname/server.crt /crt/ca.crt > /crt/$commonname/server.crt.tmp
-      mv /crt/$commonname/server.crt.tmp /crt/$commonname/server.crt
-      rm /crt/$commonname/server.csr
+      cat /crt/$domain/server.crt /crt/ca.crt > /crt/$domain/server.crt.tmp
+      mv /crt/$domain/server.crt.tmp /crt/$domain/server.crt
+      rm /crt/$domain/server.csr
 
     ;;
 
